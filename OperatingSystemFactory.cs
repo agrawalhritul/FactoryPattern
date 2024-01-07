6d5012cec3cce43b6937a2eea895ee9a82d2f509
Hritul Agrawal <hritulagrawal99@gmail.com>
2024-01-07 13:54:48 +0530

    Add project files.

---
+++ OperatingSystemFactory.cs
@@ -0,0 +1,18 @@
+﻿using OperatingSystem;
+
+namespace FactoryPattern;
+
+public class OperatingSystemFactory
+{
+    public static IOperatingSystem? GetOperatingSystemByManufacturer(string manufacturer)
+    {
+        return manufacturer.ToLower() switch
+        {
+            "google" => new Android(),
+            "microsoft" => new Windows(),
+            "apple" => new MacOS(),
+            "oracle" => new Solaris(),
+            _ => null
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OperatingSystem;
+
+namespace FactoryPattern;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the Operating System manufacturer's name");
+        string manufacturer = Console.ReadLine()!;
+        IOperatingSystem? os = OperatingSystemFactory.GetOperatingSystemByManufacturer(manufacturer);
+        string details;
+        if (os == null)
+            details = "We do not have any operating system from this manufacturer in our accout";
+        else
+            details = os.PrintDetails();
+        Console.WriteLine(details);
+    }
+}
